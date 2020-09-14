25f3c5659f851279f26fd438853776c6c03601ed
Yogesh Bhandari <fireygenius@gmail.com>
2020-09-15 00:28:37 +0545

    User Types and products added

--- products/models.py
+++ products/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from users.models import Seller, User, Customer
 
-# Create your models here.
+class Product(models.Model):
+    seller = models.OneToOneField(Seller,
+        on_delete=models.CASCADE, unique=True)
+    name = models.CharField(max_length=50, default='')
+    description = models.CharField(max_length=550, default='')
+    price = models.IntegerField(default=0)
+    quantity = models.IntegerField(default=0)
+
+
+class Rating(models.Model):
+    user_id = models.OneToOneField(Customer,
+        on_delete=models.CASCADE)
+    product_id = models.OneToOneField(Product,
+        on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+    class Meta:
+        unique_together = ['user_id', 'product_id']
--- users/models.py
+++ users/models.py
@@ -5,18 +5,19 @@ from django.utils.timezone import now
 
 
 ROLES = (
-    ('customer', 'CUSTOMER'),
-    ('seller', 'SELLER'),
-    ('support','SUPPORT'),
-    ('admin', 'ADMIN')
+    ('customer', 'customer'),
+    ('seller', 'seller'),
+    ('support','support'),
+    ('admin', 'admin')
 )
 
 def upload_dp_to(instance, filename):
      filename_base, filename_ext = os.path.splitext(filename)
-     return f'UserData/photo/{now().strftime("%Y%m%d")+filename_ext}'
+     return f'UserData/display_picture/{now().strftime("%Y%m%d")+filename_ext}'
 
 
 class User(AbstractUser):
+    phone_number = models.CharField(max_length=15, default='')
     display_picture = models.FileField(upload_to=upload_dp_to, default='')
     role = models.CharField(
         max_length=15, choices=ROLES, default='customer', null=False, blank=False)
@@ -29,5 +30,53 @@ class User(AbstractUser):
     EMAIL_FIELD = 'email'
     USERNAME_FIELD = 'email'
 
+    def __str__(self):
+        return self.email
+
+    @property
+    def is_customer(self):
+        if self.role:
+            if self.role == 'customer':
+                return True
+        return False
+
+    @property
+    def is_seller(self):
+        if self.role:
+            if self.role == 'seller':
+                return True
+        return False
+
+    @property
+    def is_support(self):
+        if self.role:
+            if self.role == 'support':
+                return True
+        return False
+    
+    @property
+    def is_admin(self):
+        if self.role:
+            if self.role == 'admin':
+                return True
+        return False
 
 
+class Customer(models.Model):
+    user_id = models.OneToOneField(User,
+        on_delete=models.CASCADE, unique=True)
+
+
+class Seller(models.Model):
+    user_id = models.OneToOneField(User,
+        on_delete=models.CASCADE, unique=True)
+
+
+class Support(models.Model):
+    user_id = models.OneToOneField(User,
+          on_delete=models.CASCADE, unique=True)
+
+
+class Admin(models.Model):
+    user_id = models.OneToOneField(User,
+          on_delete=models.CASCADE, unique=True)
